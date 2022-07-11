cc406d7ff5b369efad129842cb07eb564d8e3740
Rakshit <70335252+spuckhafte@users.noreply.github.com>
2022-07-11 22:45:00 +0530

    :db: => mongodb as database, ready but not synced

---
+++ models/channel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const channelSchema = new mongoose.Schema({
+    messages: {
+        type: String
+    }
+});
+
+const Channel = mongoose.model('Channel', channelSchema);
+
+module.exports = Channel;
